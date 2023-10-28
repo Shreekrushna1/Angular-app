33d2d29f22b89224999f8980f0bcfd85e38541ca
Shreekrushna1 <56877910+Shreekrushna1@users.noreply.github.com>
2023-10-28 17:00:32 +0530

    Add files via upload

--- routes/index.js
+++ routes/index.js
@@ -2,26 +2,64 @@ var express = require('express');
 var router = express.Router();
 var userModel = require('./users');
 const jwt = require('jsonwebtoken');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://krushnaproducts:Krushna11@cluster0.uvurvqw.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
 /* GET home page. */
 router.get('/', function (req, res, next) {
     res.render('index', { title: 'Express' });
 });
 
 router.post('/register', async function (req, res) {
-    const user = await userModel.create({
-        username: req.body.username,
-        firstName: req.body.fname,
-        lastName: req.body.lname,
-        email: req.body.email,
-        password: req.body.password,
-        isLogin: req.body.isLogin,
-    });
-    jwt.sign({ user: user }, 'secretkey', (err, token) => {
-        res.json({
-            token,
-            user,
-        });
-    });
+
+  try {
+    await client.connect();
+    console.log('Connected to the database');
+
+    const database = client.db('users'); // Replace with your database name
+    const collection = database.collection('users'); // Replace with your collection name
+
+    const { username, email, password,fname, lname } = req.body;
+
+    // Check if the user already exists
+    const existingUser = await collection.findOne({ email });
+    if (existingUser) {
+      return res.status(400).json({ error: 'User already exists' });
+    }
+
+    // Insert the new user
+    const result = await collection.insertOne({ username, email, password, fname , lname });
+    console.log(`${result.insertedCount} documents were inserted with the _id: ${result.insertedId}`);
+
+    res.status(200).json({ message: 'User registered successfully' });
+  } catch (error) {
+    console.error('Error: ', error);
+    res.status(500).json({ error: 'Server error' });
+  } finally {
+    await client.close();
+  }
+
+
+    // const user = await userModel.create({
+    //     username: req.body.username,
+    //     firstName: req.body.fname,
+    //     lastName: req.body.lname,
+    //     email: req.body.email,
+    //     password: req.body.password,
+    //     isLogin: req.body.isLogin,
+    // });
+    // jwt.sign({ user: user }, 'secretkey', (err, token) => {
+    //     res.json({
+    //         token,
+    //         user,
+    //     });
+    // });
 });
 router.get('/getAllUsers', async function (req, res) {
     const allUsers = await userModel.find();
--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,19 @@
 const mongoose = require('mongoose');
-mongoose.connect("mongodb://127.0.0.1:27017/users");
+// mongoose.connect("mongodb+srv://krushnaproducts:Krushna@11@cluster0.uvurvqw.mongodb.net/?retryWrites=true&w=majority");
+
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://krushnaproducts:Krushna11@cluster0.uvurvqw.mongodb.net/?retryWrites=true&w=majority";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
 
 const userSchema = mongoose.Schema({
   username: String,
@@ -11,3 +25,22 @@ const userSchema = mongoose.Schema({
 });
 
 module.exports = mongoose.model("users", userSchema);
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    const database = client.db('users'); // Replace with your database name
+    const collection = database.collection('users'); // Replace with your collection name
+
+    // Data to be inserted
+    // const data = { name: 'John Doe', age: 30, email: 'johndoe@example.com' };
+
+    // Insert the data
+    // const result = await collection.insertOne(data);
+    // console.log(result);
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
